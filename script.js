ec1b905c63485b2c21f2360b713853d6d36fdfd7
Jaen <jaennova9@gmail.com>
2023-06-20 00:24:09 -0600

    bugs fixed

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-document.querySelectorAll('.tag-button').forEach(button => {
-  // Agregar un evento de click al botón "Ver más"
-document.querySelector('#more-button').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const moreButton = document.querySelector('#more-button');
   const extraButtons = document.querySelectorAll('.extra-button');
+  const tagButtons = document.querySelectorAll('.tag-button');
 
-  button.addEventListener('click', () => {
-    const tag = button.dataset.tag;
-    localStorage.setItem('selectedTag', tag); // Guardar la etiqueta seleccionada en el localStorage
-    filterCards(tag);
-  });
-});
-
-// Función para filtrar las tarjetas según la etiqueta seleccionada
-function filterCards(tag) {
-  document.querySelectorAll('.card-container').forEach(card => {
-    const cardTags = card.dataset.tag.split(' ');
-    if (tag === 'all' || cardTags.includes(tag)) {
-      card.style.display = 'block';
+  // Agregar un evento de click al botón "Ver más"
+  moreButton.addEventListener('click', () => {
+    if (extraButtons[0].style.display === 'none') {
+      extraButtons.forEach(button => {
+        button.style.display = 'inline-block';
+      });
+      moreButton.textContent = 'Mostrar menos';
     } else {
-      card.style.display = 'none';
+      extraButtons.forEach(button => {
+        button.style.display = 'none';
+      });
+      moreButton.textContent = 'Ver más';
     }
   });
-}
 
-// Al cargar la página, comprobar si hay una etiqueta seleccionada guardada en el localStorage
-window.onload = function() {
+  // Agregar eventos de click a los botones de etiqueta
+  tagButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const tag = button.dataset.tag;
+      localStorage.setItem('selectedTag', tag); // Guardar la etiqueta seleccionada en el localStorage
+      filterCards(tag);
+    });
+  });
+
+  // Función para filtrar las tarjetas según la etiqueta seleccionada
+  function filterCards(tag) {
+    document.querySelectorAll('.card-container').forEach(card => {
+      const cardTags = card.dataset.tag.split(' ');
+      if (tag === 'all' || cardTags.includes(tag)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  }
+
+  // Al cargar la página, comprobar si hay una etiqueta seleccionada guardada en el localStorage
   const selectedTag = localStorage.getItem('selectedTag');
   if (selectedTag) {
     filterCards(selectedTag);
   }
-}
-// Si los botones extra están ocultos, mostrarlos y cambiar el texto del botón a "Mostrar menos"
-if (extraButtons[0].style.display === 'none') {
-  extraButtons.forEach(button => {
-    button.style.display = 'inline-block';
-  });
-  moreButton.textContent = 'Mostrar menos';
-}
-// Si los botones extra están visibles, ocultarlos y cambiar el texto del botón a "Ver más"
-else {
-  extraButtons.forEach(button => {
-    button.style.display = 'none';
-  });
-  moreButton.textContent = 'Mostrar mas';
-}
 });
