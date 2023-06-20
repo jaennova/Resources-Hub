1b971b27b6dc646ad2bd8351ae16f9357244248d
Jaen <jaennova9@gmail.com>
2023-06-20 00:08:43 -0600

    se agrego una nueva funcionalidad
    se agrego la funcionalidad para que la seleccion de tarjetas siga activa despues de que se haya actualizado la pagina

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 document.querySelectorAll('.tag-button').forEach(button => {
-    button.addEventListener('click', () => {
-      const tag = button.dataset.tag;
-      document.querySelectorAll('.card').forEach(card => {
-        if (tag === 'all' || card.dataset.tag === tag) {
-          card.style.display = 'block';
-        } else {
-          card.style.display = 'none';
-        }
-      });
-    });
+  button.addEventListener('click', () => {
+    const tag = button.dataset.tag;
+    localStorage.setItem('selectedTag', tag); // Guardar la etiqueta seleccionada en el localStorage
+    filterCards(tag);
   });
-  
+});
+
+// Función para filtrar las tarjetas según la etiqueta seleccionada
+function filterCards(tag) {
+  document.querySelectorAll('.card-container').forEach(card => {
+    const cardTags = card.dataset.tag.split(' ');
+    if (tag === 'all' || cardTags.includes(tag)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
+// Al cargar la página, comprobar si hay una etiqueta seleccionada guardada en el localStorage
+window.onload = function() {
+  const selectedTag = localStorage.getItem('selectedTag');
+  if (selectedTag) {
+    filterCards(selectedTag);
+  }
+}
+
