b4b1d69be2cf363df5700bcb231510e3937047f8
Jaen <jaennova9@gmail.com>
2023-06-21 17:25:13 -0600

    add README and JS fixed

---
+++ README.md
@@ -0,0 +1,17 @@
+# Resources Hub
+
+Resources Hub es una página web que ofrece una colección de recursos en diferentes categorías. Puedes explorar y descubrir recursos relacionados con el aprendizaje del inglés, documentación de programación, y práctica de programación.
+
+## Características
+
+- Muestra una lista de recursos organizados en tarjetas.
+- Permite filtrar los recursos por categoría haciendo clic en los botones de etiquetas.
+- Proporciona una experiencia visual agradable con un tema oscuro predeterminado.
+- Cada tarjeta de recurso contiene una imagen, título y descripción.
+- Los recursos se abren en una nueva pestaña al hacer clic en ellos.
+
+## Capturas de pantalla
+
+![Captura de pantalla 1](/assets/screen-web.png)
+
+![Captura de pantalla 2](/assets/screen-web2.png)
--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const moreButton = document.querySelector('#more-button');
-  const extraButtons = document.querySelectorAll('.extra-button');
-  const tagButtons = document.querySelectorAll('.tag-button');
-
-  // Agregar un evento de click al botón "Ver más"
-  moreButton.addEventListener('click', () => {
-    if (extraButtons[0].style.display === 'none') {
-      extraButtons.forEach(button => {
-        button.style.display = 'inline-block';
-      });
-      moreButton.textContent = 'Mostrar menos';
-    } else {
-      extraButtons.forEach(button => {
-        button.style.display = 'none';
-      });
-      moreButton.textContent = 'Ver más';
-    }
-  });
-
-  // Agregar eventos de click a los botones de etiqueta
-  tagButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const tag = button.dataset.tag;
-      localStorage.setItem('selectedTag', tag); // Guardar la etiqueta seleccionada en el localStorage
-      filterCards(tag);
-    });
-  });
-
-  // Función para filtrar las tarjetas según la etiqueta seleccionada
-  function filterCards(tag) {
-    document.querySelectorAll('.card-container').forEach(card => {
-      const cardTags = card.dataset.tag.split(' ');
-      if (tag === 'all' || cardTags.includes(tag)) {
-        card.style.display = 'block';
-      } else {
-        card.style.display = 'none';
-      }
-    });
+document.querySelectorAll('.tag-button').forEach(button => {
+  // Si el botón es el botón "Ver más", no aplicar la funcionalidad de filtrado
+  if (button.dataset.tag === 'mas-secciones') {
+    return;
   }
 
-  // Al cargar la página, comprobar si hay una etiqueta seleccionada guardada en el localStorage
+  button.addEventListener('click', () => {
+    const tag = button.dataset.tag;
+    localStorage.setItem('selectedTag', tag); // Guardar la etiqueta seleccionada en el localStorage
+    filterCards(tag);
+  });
+});
+
+// Función para filtrar las tarjetas según la etiqueta seleccionada
+function filterCards(tag) {
+  document.querySelectorAll('.card-container').forEach(card => {
+    const cardTags = card.dataset.tag.split(' ');
+    if (tag === 'all' || cardTags.includes(tag)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
+// Al cargar la página, comprobar si hay una etiqueta seleccionada guardada en el localStorage
+window.onload = function() {
   const selectedTag = localStorage.getItem('selectedTag');
   if (selectedTag) {
     filterCards(selectedTag);
   }
-});
+}
